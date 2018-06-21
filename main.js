bc75766b17812efffdede208b4f40bafb52acab0
nghiavo2512 <nghiavlg123@gmail.com>
2018-06-21 20:30:45 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function playStream(idVideoTag, stream){
 }
 // openStream()
 // .then(stream=> playStream('localStream',stream));
- const peer= new Peer({host:'mypeer2512.herokuapp.com/',secure: true, port:443});
+ const peer= new Peer({key: 'peerjs' ,host:'mypeer2512.herokuapp.com/',secure: true, port:443});
  peer.on('open',id=>{
  	$('#my-peer').append(id);
  	$('#btnSignUp').click(()=>{
